45b1ab802394636ac2ebd2397cae339c01351489
vishnu2981997 <vishnu2981997@gmail.com>
2021-06-22 13:19:05 +0530

    added global error handlers

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const httpStatus = require('http-status');
 const routes = require('./routes/v1');
+const ApiError = require('./utils/ApiError');
+const {errorConverter, errorHandler} = require('./middlewares/error');
 
 const app = express();
 // set security HTTP headers
@@ -33,5 +36,16 @@ app.get('/', (req, res) => {
 });
 
 app.use('/v1', routes);
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+    next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
 
 module.exports = app;
